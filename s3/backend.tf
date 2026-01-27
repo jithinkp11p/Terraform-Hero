fe3ec7e69bbf1bc8161f1712280a936bd57faecb
jithinkp11p <81610302+jithinkp11p@users.noreply.github.com>
2026-01-27 22:26:59 +0530

    Change S3 bucket name in backend.tf
    Updated S3 bucket name for Terraform backend configuration.

--- s3/backend.tf
+++ s3/backend.tf
@@ -1,7 +1,7 @@
 # Configure the S3 backend for Terraform state storage 
 terraform {
   backend "s3" {
-    bucket = "lokibucket1994"
+    bucket = "******bucketname******"
     key    = "terraform/aws-terraform-state-files"
     region = "us-east-2"
     use_lockfile = true
